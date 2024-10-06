463f05ec7b28bf0a3b333722395b0d89cd97d1ba
Somen Rajak <somen1228@gmail.com>
2024-10-06 22:13:01 +0530

    nav layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { FaBars, FaTimes, FaSun, FaMoon } from "react-icons/fa";
+import { FaBars, FaTimes, FaMoon } from "react-icons/fa";
 import { PiSunDim } from "react-icons/pi";
-import profilePicture from "../assets/profile-pic.jpg"
+import profilePicture from "../assets/profile-pic.jpg";
 
-const Navbar = ({
-  activeSection,
-  scrollToSection,
-  toggleDarkMode,
-  darkMode,
-}) => {
+const Navbar = ({ activeSection, scrollToSection, toggleDarkMode, darkMode }) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
@@ -17,78 +12,83 @@ const Navbar = ({
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-opacity-90 backdrop-filter backdrop-blur-lg">
-      <div className="container mx-auto px-6 py-3">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-6">
-            <div className="absolute rounded-full left-6">
-                <img src={profilePicture} alt="profile-pic" className="rounded-full w-10 h-10  aspect-auto self-center"/>
-            </div>
-            <motion.h1
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-opacity-90 backdrop-blur-lg w-full">
+      <div className="w-full flex justify-between items-center px-4 lg:px-8 py-3">
+        {/* Left: Profile */}
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="flex items-center gap-4"
+        >
+          <img
+            src={profilePicture}
+            alt="profile-pic"
+            className="rounded-full w-10 h-10"
+          />
+          <h2 className="text-xl font-bold">Somen Rajak</h2>
+        </motion.div>
+
+        {/* Middle: Links for larger screens */}
+        <div className="hidden md:flex items-center space-x-6">
+          {["home", "projects", "skills", "contact"].map((section) => (
+            <motion.button
+              key={section}
+              onClick={() => scrollToSection(section)}
+              className={`text-sm font-medium transition-all duration-300 ${
+                activeSection === section ? "text-blue-500" : "text-gray-400 hover:text-white"
+              }`}
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5 }}
-              className="text-2xl font-bold md:pl-12 pl-16"
             >
-              Somen Rajak
-            </motion.h1>
-          </div>
-          <div className="hidden md:flex items-center space-x-4">
-            {["home", "projects", "skills", "contact"].map((section) => (
-              <motion.button
-                key={section}
-                onClick={() => scrollToSection(section)}
-                className={`px-3 py-2 rounded-md text-sm font-medium ${
-                  activeSection === section ? "text-blue-500" : "text-gray-400"
-                }`}
-                initial={{ opacity: 0, y: -20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-              >
-                {section.charAt(0).toUpperCase() + section.slice(1)}
-              </motion.button>
-            ))}
-            <button
-              onClick={toggleDarkMode}
-              className="px-3 py-2 rounded-md text-sm font-medium text-gray-400 hover:text-blue-500 transition duration-300"
-            >
-              {darkMode ? <PiSunDim size={35} /> : <FaMoon size={20} />}
-            </button>
-          </div>
-          <div className="md:hidden flex items-center space-x-4">
-            <button
-              onClick={toggleDarkMode}
-              className="px-3 py-2 rounded-md text-sm font-medium text-gray-400 hover:text-blue-500 transition duration-300"
-            >
-              {darkMode ? <PiSunDim size={35} /> : <FaMoon size={20} />}
-            </button>
-            <button onClick={toggleMobileMenu} className="text-gray-700">
-              {isMobileMenuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
-            </button>
-          </div>
+              {section.charAt(0).toUpperCase() + section.slice(1)}
+            </motion.button>
+          ))}
+          <button
+            onClick={toggleDarkMode}
+            className="text-gray-400 hover:text-blue-500 transition duration-300"
+          >
+            {darkMode ? <PiSunDim size={35} /> : <FaMoon size={20} />}
+          </button>
         </div>
-        {isMobileMenuOpen && (
-          <div className="md:hidden mt-4">
-            {["home", "projects", "skills", "contact"].map((section) => (
-              <motion.button
-                key={section}
-                onClick={() => {
-                  scrollToSection(section);
-                  toggleMobileMenu();
-                }}
-                className={`block w-full text-left px-3 py-2 rounded-md text-sm font-medium ${
-                  activeSection === section ? "text-blue-500" : "text-gray-400"
-                }`}
-                initial={{ opacity: 0, y: -20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-              >
-                {section.charAt(0).toUpperCase() + section.slice(1)}
-              </motion.button>
-            ))}
-          </div>
-        )}
+
+        {/* Right: Mobile Menu */}
+        <div className="md:hidden flex items-center space-x-4">
+          <button
+            onClick={toggleDarkMode}
+            className="text-gray-400 hover:text-blue-500 transition duration-300"
+          >
+            {darkMode ? <PiSunDim size={35} /> : <FaMoon size={20} />}
+          </button>
+          <button onClick={toggleMobileMenu} className="text-gray-700">
+            {isMobileMenuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+          </button>
+        </div>
       </div>
+
+      {/* Mobile menu */}
+      {isMobileMenuOpen && (
+        <div className="md:hidden mt-4 space-y-4 px-4">
+          {["home", "projects", "skills", "contact"].map((section) => (
+            <motion.button
+              key={section}
+              onClick={() => {
+                scrollToSection(section);
+                toggleMobileMenu();
+              }}
+              className={`block w-full text-left px-3 py-2 text-sm font-medium transition-all duration-300 ${
+                activeSection === section ? "text-blue-500" : "text-gray-400"
+              }`}
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              {section.charAt(0).toUpperCase() + section.slice(1)}
+            </motion.button>
+          ))}
+        </div>
+      )}
     </nav>
   );
 };
